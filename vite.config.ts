48cf7f8e13e8749c85046ae1f7aacbfa86b3a8e5
hwanheejung <hwanheejung117@gmail.com>
2025-05-10 00:41:36 +0900

    chore: tanstack router 도입

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from "node:path";
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    TanStackRouterVite({
+      routesDirectory: "src/pages",
+      generatedRouteTree: "src/app/routes/routeTree.gen.ts",
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    react(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  publicDir: "public",
+  cacheDir: ".vite",
+});
