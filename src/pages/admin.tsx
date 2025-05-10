a35f8011e01c0eceb9825f325fed1c3e75f6fc36
hwanheejung <hwanheejung117@gmail.com>
2025-05-11 00:23:05 +0900

    🎨 코드 정리

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,4 +1,5 @@
 import { AdminGame } from '@/widgets/admin-game';
+import { AdminReady } from '@/widgets/admin-ready';
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/admin')({
@@ -8,17 +9,24 @@ export const Route = createFileRoute('/admin')({
 function RouteComponent() {
   return (
     <div className="h-dvh flex flex-col max-w-[1400px] mx-auto">
-      <header className="flex items-center justify-start bg-transparent">
-        <img src="/images/logo.png" alt="쉐킷투유 로고" className="w-60 object-center" />
-      </header>
-      <div className="p-4 h-full flex flex-col">
-        <img
-          src="/images/toyou_bg.gif"
-          alt="toyou_bg"
-          className="fixed inset-0 w-full h-full object-cover opacity-80 -z-10"
-        />
-        <AdminGame />
-      </div>
+      <AdminReady />
+      <GamePage />
     </div>
   );
 }
+
+const GamePage = () => (
+  <>
+    <header className="flex items-center justify-start bg-transparent">
+      <img src="/images/logo.png" alt="쉐킷투유 로고" className="w-60 object-center" />
+    </header>
+    <div className="p-4 h-full flex flex-col">
+      <img
+        src="/images/toyou_bg.gif"
+        alt="toyou_bg"
+        className="fixed inset-0 w-full h-full object-cover opacity-80 -z-10"
+      />
+      <AdminGame />
+    </div>
+  </>
+);
