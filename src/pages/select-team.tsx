c4979a61b1bae769cc5371b41be3da49e6a16067
hwanheejung <hwanheejung117@gmail.com>
2025-05-10 17:40:05 +0900

    💄 팀 선택 화면 구현

--- src/pages/select-team.tsx
+++ src/pages/select-team.tsx
@@ -1,8 +1,14 @@
+import { TeamSelectForm } from '@/widgets/team-select';
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/select-team')({
   component: RouteComponent,
 });
+
 function RouteComponent() {
-  return <div>Hello "/select-team"!</div>;
+  return (
+    <div className="flex items-center justify-center min-h-dvh bg-cool-gray-0">
+      <TeamSelectForm />
+    </div>
+  );
 }
