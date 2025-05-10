21f7b56f08fdf8dea570e9ac9b94454511f55758
Yeonu-Kim <ywk0524@snu.ac.kr>
2025-05-11 06:42:12 +0900

    🔥 등록 시간 삭제

--- src/widgets/game/game.tsx
+++ src/widgets/game/game.tsx
@@ -14,7 +14,6 @@ export const Game = () => {
 
   return (
     <div className="min-h-screen w-full flex flex-col justify-center items-center gap-8">
-      <p>{gameEndTime}</p>
       <GameEndCalculator setGameEndTime={setGameEndTime} setResultBeatList={setResultBeatList} />
       <GameResultSender
         gameEndTime={gameEndTime}
