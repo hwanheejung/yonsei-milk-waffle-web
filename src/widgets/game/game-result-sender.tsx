72e953ce276d028bea1277c5b7f54ea7ecf164f3
Yeonu-Kim <ywk0524@snu.ac.kr>
2025-05-11 06:41:01 +0900

    ✨ 결과 페이지

--- src/widgets/game/game-result-sender.tsx
+++ src/widgets/game/game-result-sender.tsx
@@ -1,13 +1,12 @@
 import { usePostGameMutation } from '@/entities/game/api/mutations';
 import { Team } from '@/entities/team';
 import type { Timestamp } from '@/entities/time/Timestamp';
-import { getCurrentUnixTime } from '@/shared/lib/date';
-import { useEffect, useRef, useState } from 'react';
-import { useNavigate } from '@tanstack/react-router';
+
+import { Button } from '@/shared/ui';
 import { setStorage } from '@/shared/lib/sessionStorage';
+import { useNavigate } from '@tanstack/react-router';
 
 export const GameResultSender = ({
-  gameEndTime,
   userBeatList,
   userBeatHistory,
 }: {
@@ -15,42 +14,17 @@ export const GameResultSender = ({
   userBeatList: Timestamp[];
   userBeatHistory: { time: number; x: number }[];
 }) => {
-  const [error, setError] = useState('');
   const { mutateAsync: sendGameResult } = usePostGameMutation();
-  const hasSentResult = useRef(false);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    if (gameEndTime === null) {
-      return;
-    }
+  const handleCloseSubmit = () => {
+    setStorage({ key: 'BEAT_LIST', value: userBeatHistory });
 
-    const interval = setInterval(() => {
-      const now = getCurrentUnixTime();
-      console.log(!hasSentResult.current && now > gameEndTime);
+    sendGameResult({
+      body: { timestamp: userBeatList, team: Team.KOREA },
+    }).then(() => {});
+    navigate({ to: '/result' });
+  };
 
-      if (!hasSentResult.current && now > gameEndTime) {
-        setStorage({ key: 'BEAT_LIST', value: userBeatHistory });
-        setError('test');
-
-        sendGameResult({
-          body: { timestamp: userBeatList, team: Team.KOREA },
-        })
-          .then(() => {
-            setError('통과');
-            navigate({ to: '/result' });
-          })
-          .catch((err) => {
-            setError(JSON.stringify(err, null, 2));
-          });
-
-        hasSentResult.current = true;
-        clearInterval(interval);
-      }
-    }, 500);
-
-    return () => clearInterval(interval);
-  }, [gameEndTime, userBeatList, sendGameResult, navigate, userBeatHistory]);
-
-  return <div>{error}</div>;
+  return <Button onClick={handleCloseSubmit}>종료하기</Button>;
 };
